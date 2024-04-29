8913d67782512158af2c04fd45d770a5aeff23dd
Alvaro Gonzalez <alvaro.gonzalez.dev@gmail.com>
2024-04-29 00:10:09 -0400

    set llm to llama3

--- open_rag.sh
+++ open_rag.sh
@@ -1,5 +1,5 @@
 export RAG_HOME=/Users/alvarogonzalez/develop/4rgs-ai-rag
-export MODEL=llama3:70b
+export MODEL=llama3
 export EMBEDDINGS_MODEL_NAME=all-MiniLM-L6-v2
 export TARGET_SOURCE_CHUNKS=5
 cd $RAG_HOME
